8fe8ea1b7ef7a646c665aae7c47a0521ade743a9
Victor Antonovich <v.antonovich@gmail.com>
2023-09-06 15:34:02 +0300

    Disable VBR encoding for MP3 output
    VBR-encoded MP3s without proper VBR headers have inconsistent actual and displayed lengths in most players.

--- src/output.cpp
+++ src/output.cpp
@@ -135,7 +135,7 @@ lame_t airlame_init(mix_modes mixmode, int highpass, int lowpass) {
 	}
 
 	lame_set_in_samplerate(lame, WAVE_RATE);
-	lame_set_VBR(lame, vbr_mtrh);
+	// lame_set_VBR(lame, vbr_mtrh);
 	lame_set_brate(lame, 16);
 	lame_set_quality(lame, 7);
 	lame_set_lowpassfreq(lame, lowpass);
